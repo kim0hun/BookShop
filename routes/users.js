d34d5f3eca9163b9de26cc78dc7f92286a8b36fa
Hun <dudgns9194@gmail.com>
2024-05-22 22:08:39 +0900

    controller 생성

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const conn = require('../mariadb');
-const {StatusCodes} = require('http-status-codes');
+const join = require('../controller/userController');
 
 router.use(express.json());
+
 // 회원가입
-router.post('/join', (req, res) => {
-    const {email, password} = req.body;
+router.post('/join', join);
 
-    let sql = 'insert into users (email, password) values (?, ?)';
-    let values = [email, password];
-    conn.query(sql, values, (err, results)=>{
-        if(err){
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        }
-        return res.status(StatusCodes.CREATED).json(results);
-    });
-});
-
+// 로그인
 router.post('/login', (req, res) => {
     res.json('로그인');
 });
 
+// 비밀번호 초기화 요청
 router.post('/reset', (req, res) => {
     res.json('비밀번호 초기화 요청');
 });
 
+// 비밀번호 초기화
 router.put('/reset', (req, res) => {
     res.json('비밀번호 초기화');
 });
