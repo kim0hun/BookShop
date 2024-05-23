b56c68be8aaf3684383bd3ecc731f0c4a395dd24
Hun <dudgns9194@gmail.com>
2024-05-23 14:49:07 +0900

    도서 전체 조회 상세 도서 조회

--- controller/bookController.js
+++ controller/bookController.js
@@ -2,11 +2,36 @@ const conn = require('../mariadb');
 const {StatusCodes} = require('http-status-codes');
 
 const allBooks = (req, res) => {
-    res.json('전체 도서 조회');
+    const sql = 'select * from books';
+    conn.query(sql, (err, results)=>{
+        if(err){
+            console.log(err);
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        }
+        if(results){
+            return res.status(StatusCodes.OK).json(results);
+        }else{
+           return res.status(StatusCodes.NOT_FOUND).json('도서 정보 없음');
+        }
+    });
 };
 
 const bookDetail = (req, res) => {
-    res.json('개별 도서 조회');
+    let {id} = req.params;
+    
+    let sql = 'select * from books where id = ?';
+    conn.query(sql, id, (err, results)=>{
+        if(err){
+            console.log(err);
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        }
+
+        if(results[0]){
+            return res.status(StatusCodes.OK).json(results[0]);
+        }else{
+            return res.status(StatusCodes.NOT_FOUND).end();
+        }
+    });
 };
 
 const booksByCategory = (req, res) => {
