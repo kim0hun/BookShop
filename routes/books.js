c73a75be5520d1191808104df4d4d9f77a1569cb
Hun <dudgns9194@gmail.com>
2024-05-23 16:49:29 +0900

    카테고리 전체 목록 조회 API 구현

--- routes/books.js
+++ routes/books.js
@@ -7,7 +7,7 @@ const {
 
 router.use(express.json());
 
-router.get('/', allBooks); // 전체 도서 조회
+router.get('/', allBooks); // {카테고리별} 전체 도서 조회
 router.get('/:id', bookDetail); // 개별 도서 조회
 
 module.exports = router;
