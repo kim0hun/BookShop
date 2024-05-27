4d77b240029886fe037df839d39bd90c4db335e0
Hun <dudgns9194@gmail.com>
2024-05-27 15:18:29 +0900

    좋아요 삭제 구현

--- controller/likeController.js
+++ controller/likeController.js
@@ -7,7 +7,7 @@ const addLike = (req, res) => {
     const { user_id } = req.body;
 
     let sql = 'insert into likes (user_id, liked_book_id) values (?, ?)';
-    let values = [ user_id, id ];
+    let values = [user_id, id];
     conn.query(sql, values, (err, results) => {
         if (err) {
             console.log(err);
@@ -20,6 +20,19 @@ const addLike = (req, res) => {
 
 const removeLike = (req, res) => {
 
+    const { id } = req.params;
+    const { user_id } = req.body;
+
+    let sql = 'DELETE FROM likes WHERE user_id = ? AND liked_book_id = ?';
+    let values = [user_id, id];
+    conn.query(sql, values, (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(StatusCodes.BAD_REQUEST).end();
+        }
+
+        return res.status(StatusCodes.OK).json(results);
+    });
 };
 
 module.exports = {
