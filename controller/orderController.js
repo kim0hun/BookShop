108345d81d84ec1555a7173e3952e616414a956a
Hun <dudgns9194@gmail.com>
2024-05-29 18:00:34 +0900

    주문하기 orderedBook insert 구현

--- controller/orderController.js
+++ controller/orderController.js
@@ -5,7 +5,7 @@ const order = (req, res) => {
 
     const {items, delivery, totalQuantity, totalPrice, userId, firstBookTitle} = req.body;
     let delivery_id = 3;
-    let order_id;
+    let order_id = 2;
 
     let sql = 'insert into delivery (address, receiver, contact) values (?, ?, ?)';
     let values = [delivery.address, delivery.receiver, delivery.contact];
@@ -23,14 +23,29 @@ const order = (req, res) => {
     sql = `insert into orders (book_title, total_quantity, total_price, user_id, delivery_id)
     values (?, ?, ?, ?, ?)`;
     values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
-    conn.query(sql, values, (err, results) => {
+    // conn.query(sql, values, (err, results) => {
+    //     if (err) {
+    //         console.log(err);
+    //         res.status(StatusCodes.BAD_REQUEST).end();
+    //     }
+
+    //     order_id = results.insertId;
+    //     console.log(order_id);
+        
+    //     return res.status(StatusCodes.OK).json(results);
+    // });
+
+    sql = `insert into orderedBook (order_id, book_id, quantity) values ?`;
+    values = [];
+    items.forEach((item) => {
+        values.push([order_id, item.book_id, item.quantity]);
+
+    });
+    conn.query(sql, [values], (err, results) => {
         if (err) {
             console.log(err);
             res.status(StatusCodes.BAD_REQUEST).end();
         }
-
-        order_id = results.insertId;
-        console.log(order_id);
         
         return res.status(StatusCodes.OK).json(results);
     });
